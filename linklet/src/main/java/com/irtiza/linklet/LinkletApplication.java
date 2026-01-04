f1755565395486a6d2cb844224c9a5cb3f88bbc6
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2026-01-04 16:34:29 +0500

    Initialize linklet backend in a monorepo style

---
+++ linklet/src/main/java/com/irtiza/linklet/LinkletApplication.java
@@ -0,0 +1,13 @@
+package com.irtiza.linklet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkletApplication.class, args);
+	}
+
+}
---
+++ linklet/src/test/java/com/irtiza/linklet/LinkletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irtiza.linklet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
